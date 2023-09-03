2c1f54599143fd5ca78eb338c7bc762684ab67ca
ottonellosanti <141082803+ottonellosanti@users.noreply.github.com>
2023-09-03 10:13:51 -0300

    Add files via upload

---
+++ Trabajando con Arrays/js/script.js
@@ -0,0 +1,47 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  // Filtra los elementos de tipo String del strangeArray
+  const stringElements = strangeArray.filter(item => typeof item === "string");
+
+  // Ordena alfabéticamente los elementos de tipo String
+  stringElements.sort();
+
+  // Llama a la función showList para mostrar los elementos en la página
+  showList(stringElements);
+});
